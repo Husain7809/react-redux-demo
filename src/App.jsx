294a88cbde536548f154d4d2b3d1b76892569f2b
Mohammadhusain <mh.varaliya@gmail.com>
2024-08-11 17:28:58 +0530

    add counter function using redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
 import React from 'react'
+import Counter from './components/Counter'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch();
   return (
     <div>
-      Hello
+      <span>
+        <Counter />
+      </span>
+      <button style={{ padding: '15px', background: 'green', border: '0', cursor: 'pointer', borderRadius: '5px', marginLeft: '10px', }}
+        onClick={e => dispatch({ type: "INCREMENT" })}>
+        Increment
+      </button>
+      <button style={{ padding: '15px', background: 'red', border: '0', cursor: 'pointer', borderRadius: '5px', marginLeft: '10px' }}
+        onClick={e => dispatch({ type: "DECREMENT" })}>
+        Decrement
+      </button>
     </div>
   )
 }
